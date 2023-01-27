c20b7b7c0754ba19f7daba03247c246dfa9c546c
Jonathan Moore <m361234@heb.com>
2023-01-27 10:42:56 -0600

    lib.hs cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,3 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use lambda-case" #-}
-{-# HLINT ignore "Redundant $" #-}
 module Lib
     (
     ) where
-import Data.Function (($))
-
-
-
-
